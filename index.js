20d36db066c0b24e03fc58904a1b1833a25f8c4b
Daniel A Koontz <dak.7997@gmail.com>
2020-06-14 21:36:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function doToElementsInArray(array, callback) {
 }
 
 // Add your changeCompletely() function here:
-function changeCompletely(element, array, index) {
+function changeCompletely(element, index, array) {
 
 }
